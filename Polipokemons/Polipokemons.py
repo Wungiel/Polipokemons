0cd66ead589bad6700cfa638801ca6ad5be90e33
Rdulemba <rdulemba@gmail.com>
2018-12-02 17:24:13 +0100

    Initial setup

--- Polipokemons/Polipokemons.py
+++ Polipokemons/Polipokemons.py
@@ -1,1 +1,12 @@
+import pygame, thorpy
 
+pygame.init()
+screen = pygame.display.set_mode((800, 600))
+done = False
+
+while not done:
+        for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                        done = True
+        
+        pygame.display.flip()
